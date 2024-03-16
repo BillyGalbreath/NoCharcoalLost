ef368902933eb00ef54a6ae27939d1e8fe05ac39
William Blake Galbreath <Blake.Galbreath@GMail.com>
2024-03-15 21:53:01 -0500

    cleanup project files

--- src/NoCharcoalLostMod.cs
+++ src/NoCharcoalLostMod.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
-using System.Reflection.Emit;
 using HarmonyLib;
 using Vintagestory.API.Common;
 using Vintagestory.API.Server;
@@ -10,29 +10,30 @@ using Vintagestory.GameContent;
 namespace NoCharcoalLost;
 
 [HarmonyPatch]
-public class NoCharcoalLost : ModSystem {
-    private Harmony harmony;
+public class NoCharcoalLostMod : ModSystem {
+    private Harmony? _harmony;
 
     public override bool ShouldLoad(EnumAppSide side) {
         return side.IsServer();
     }
 
     public override void StartServerSide(ICoreServerAPI capi) {
-        harmony = new Harmony(Mod.Info.ModID);
-        harmony.PatchAll(Assembly.GetExecutingAssembly());
+        _harmony = new Harmony(Mod.Info.ModID);
+        _harmony.PatchAll(Assembly.GetExecutingAssembly());
     }
 
     public override void Dispose() {
-        harmony?.UnpatchAll(Mod.Info.ModID);
+        _harmony?.UnpatchAll(Mod.Info.ModID);
     }
 
     [HarmonyTranspiler]
     [HarmonyPatch(typeof(BlockEntityCharcoalPit), "ConvertPit")]
-    public static IEnumerable<CodeInstruction> ConvertPit(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
-        var codes = new List<CodeInstruction>(instructions);
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static IEnumerable<CodeInstruction> ConvertPit(IEnumerable<CodeInstruction> instructions) {
+        List<CodeInstruction> codes = new(instructions);
 
         for (int i = 0; i < codes.Count; i++) {
-            if (!codes[i]?.operand?.ToString()?.Equals("0.125") ?? true) {
+            if (!codes[i].operand?.ToString()?.Equals("0.125") ?? true) {
                 continue;
             }
 
